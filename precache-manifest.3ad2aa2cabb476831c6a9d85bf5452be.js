eb704b9f6dcc8c440b8ca0fd748affa195255d3a
AxelZeta <axelzanella93@gmail.com>
2019-04-13 10:58:35 +0200

    Updates

---
+++ precache-manifest.3ad2aa2cabb476831c6a9d85bf5452be.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "40f1c01ae427b8d68f3039219a1e6a27",
+    "url": "/My-first-React-App/static/media/crewniverse_font.40f1c01a.woff"
+  },
+  {
+    "revision": "4fef8522d0d0e386e135",
+    "url": "/My-first-React-App/static/js/runtime~main.2d5dd50e.js"
+  },
+  {
+    "revision": "f8e6765fd91f8ed7e603",
+    "url": "/My-first-React-App/static/js/main.c32497da.chunk.js"
+  },
+  {
+    "revision": "54029de5a8d54107eeee",
+    "url": "/My-first-React-App/static/js/2.1a543e43.chunk.js"
+  },
+  {
+    "revision": "f8e6765fd91f8ed7e603",
+    "url": "/My-first-React-App/static/css/main.da1a1931.chunk.css"
+  },
+  {
+    "revision": "54029de5a8d54107eeee",
+    "url": "/My-first-React-App/static/css/2.bf34dff7.chunk.css"
+  },
+  {
+    "revision": "be0a19570efaf79510d8c1b555e24c7c",
+    "url": "/My-first-React-App/index.html"
+  }
+];
